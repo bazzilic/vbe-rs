7af6245f4c14ae840d466d42743e83132950e37a
bazzilic <bazzilic@gmail.com>
2023-11-22 00:49:54 +0800

    preparing for crates.io publication

--- src/decode.rs
+++ src/decode.rs
@@ -1,3 +1,13 @@
+/// Decode a variable-length integer from a byte stream.
+///
+/// Since we don't know the size of the integer, we use a `u128` to store it.
+///
+/// # Example
+/// ```rust
+/// use vbe::decode;
+/// let res: u128 = decode(vec![0x7f, 0x9f].into_iter()).unwrap();
+/// assert_eq!(res, 0x0fffu128);
+/// ```
 pub fn decode(mut i: impl Iterator<Item=u8>) -> Result<u128, String> {
     let mut res = 0u128;
 
--- src/encode.rs
+++ src/encode.rs
@@ -1,6 +1,14 @@
 use num_traits::{PrimInt, Unsigned};
 use std::mem::size_of;
 
+/// Encode an integer into a variable-length byte array.
+///
+/// # Example
+/// ```rust
+/// use vbe::encode;
+/// let res: Vec<u8> = encode(&22u32);
+/// assert_eq!(res, vec![22u8 | 0x80]);
+/// ```
 pub fn encode<T>(i: &T) -> Vec<u8>
 where
     T: PrimInt + Unsigned + Copy,
